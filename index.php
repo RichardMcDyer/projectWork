6a1ff2e3994e5b411f7ec5cd40182830fa609184
x15366381 <doc08@live.ie>
2016-12-01 13:37:11 +0000

    index.php
    yaaaaaaaaaaaaaa

--- index.php
+++ index.php
@@ -1,14 +1,45 @@
 <?php
-  $xml = new DOMDocument;
-  $xml->load('shoes.xml');
+/*
+*Using SimpleXML to update xml data
+*/
+//prevent from redirect
+if(isset($_SERVER['HTTP_REFERER'])){
+  header("Location: ".$_SERVER['HTTP_REFERER']);
+}
+else{
+  echo "Error";
+}
+//store form data in variables
+$brand = $_POST["brand"];
+$name = $_POST["name"]
+$price = $_POST["price"];
+$type = $_POST["type"];
+$colour = $_POST["colour"];
+$gender = $_POST["gender"];
+$minSize = $_POST["smallest size"];
+$maxSize = $_POST["largest size"];
+$image = $_POST["image"]
 
-  $xsl = new DOMDocument;
-  $xsl->substituteEntities = true;
-  $xsl->load('shoes.xsl');
-
-  $proc=new XSLTProcessor;
-
-  $proc->importStyleSheet($xsl);
-
-  echo $proc->transformToXML($xml);
+//check if xml file exists
+if(file_exists('shoes.xml')){
+  //load data from xml file into xml variable
+  $xml = simplexml_load_file('shoes.xml');
+  //add new book element to variable
+  $newChild = $xml->addChild('store');
+  $newChild->addChild('brand',$brand);
+  $newChild->addChild('name',$name);
+  $newChild->addChild('price',$price);
+  $newChild->addChild('type',$type);
+  $newChild->addChild('colour',$colour);
+  $newChild->addChild('gender',$gender);
+  $newChild->addChild('minSize',$minSize);
+  $newChild->addChild('maxSize',$maxSize);
+  $newChild->addChild('image',$image);
+}
+//if file doesnt exist
+else{
+  exit('Failed to open shoes.xml');//error message
+}
+//save new data from variable back into an xml file
+file_put_contents('/home/cabox/workspace/project/shoes.xml',$xml->asXML());
 ?>
